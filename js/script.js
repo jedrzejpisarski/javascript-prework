433ff32a2cf544e3215f34cc2bf58f31c217b0da
Jędrzej Pisarski <jedrzej.pisarski@wp.pl>
2020-02-11 16:15:52 +0100

    Js_change4

--- js/script.js
+++ js/script.js
@@ -27,13 +27,44 @@ if(playerInput == '1'){
 }
 else if(playerInput == '2'){
     playerMove = 'papier';
-  }
+}
 else if(playerInput == '3'){
     playerMove = 'nożyce';
-  }
+}
+
+let playerMove = 'nieznany ruch';
 
 printMessage('Twój ruch to: ' + playerMove);
 
 if( computerMove == 'kamień' && playerMove == 'papier'){
     printMessage('Ty wygrywasz!');
-  }
+}
+
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+}
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+
+}
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+}
+if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+}
+if( computerMove == 'Nożyce' && playerMove == 'Nożyce'){
+    printMessage('Remis!');
+}
+
+
+if( computerMove == 'kamień' && playerMove == 'Nożyce'){
+    printMessage('Porażka');
+}
+if( computerMove == 'Nożyce' && playerMove == 'Papier'){
+    printMessage('Porażka');
+}
+if( computerMove == 'Papier' && playerMove == 'Kamień'){
+    printMessage('Porażka');
+}
+
