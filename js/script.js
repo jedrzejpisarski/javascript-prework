16f60b83533efed0072ffc4bb48665ed26e76820
jedrzejpisarski <60627936+jedrzejpisarski@users.noreply.github.com>
2020-02-19 17:52:51 +0100

    Update Script Js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    }
     if(argMoveId == 2){
       return 'papier';
+    }
     if(argMoveId == 3){
-      return 'nożyce';        
+      return 'nożyce'; 
+    }       
 }
 function displayResult(argComputerMove, argPlayerMove);
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
